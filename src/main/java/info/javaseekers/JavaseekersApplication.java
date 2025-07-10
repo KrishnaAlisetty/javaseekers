2ba5b911f6e7aa52b37653e8345eacf6f569ca90
Krishna Alisetty <krishnaalisetty@gmail.com>
2025-07-10 13:56:16 +0530

    initial commit

---
+++ src/main/java/info/javaseekers/JavaseekersApplication.java
@@ -0,0 +1,13 @@
+package info.javaseekers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseekersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaseekersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/javaseekers/JavaseekersApplicationTests.java
@@ -0,0 +1,13 @@
+package info.javaseekers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaseekersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
